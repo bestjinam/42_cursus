3a2ca116c7e0ccad51303e1e528fb4471790d3e3
bestjinam <jinam@student.42seoul.kr>
2022-11-16 18:44:06 +0900

    new pipex

---
+++ pipex_first/config/variables.mk
@@ -0,0 +1,20 @@
+# library
+LIB_NAME = libft.a
+LIB_DIR = libft
+LIB = $(LIB_DIR)/$(LIB_NAME)
+
+# file name
+
+M_FILES =			pipe_error_handling \
+					pipe_main \
+					pipe_parsing_args
+
+B_FILES =			pipe_error_handling_bonus \
+					pipe_main_bonus \
+					pipe_bonus \
+					pipe_parsing_args_bonus
+# file directory
+
+OUT_DIR = 			objcs
+B_DIR =				bonus_pipex
+M_DIR =				mandatory_pipex
