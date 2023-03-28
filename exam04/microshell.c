2fc3e2fdbb5e05dae74e85f360228a35af4bd9c6
bestjinam <jinam@student.42seoul.kr>
2023-03-28 15:35:53 +0900

    exam04

---
+++ exam04/microshell.c
@@ -0,0 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   microshell.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jinam <jinam@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/19 20:45:21 by jinam             #+#    #+#             */
+/*   Updated: 2023/03/28 11:24:51 by jinam            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <string.h>
+
+#ifdef TEST_SH
+# define TEST		1
+#else
+# define TEST		0
+#endif
+
+int	ft_putstr_fd(char *str, char *arg)
+{
+	while (*str)
+		write(2, str++, 1);
+	if (arg)
+		while (*arg)
+			write(2, arg++, 1);
+	write(2, "\n", 1);
+	return (1);
+}
+
+int	ft_execute(char *argv[], int i, int tmp_fd, char **env)
+{
+	argv[i] = NULL;
+	dup2(tmp_fd, STDIN_FILENO);
+	close(tmp_fd);
+	execve(argv[0], argv, env);
+	return (ft_putstr_fd("error: cannot execute ", argv[0]));
+}
+
+int	main(int argc, char **argv, char **env)
+{
+	int	i;
+	int	fd[2];
+	int	tmp_fd;
+
+	(void) argc;
+	i = 0;
+	tmp_fd = dup(STDIN_FILENO);
+	while (argv[i] && argv[i + 1])
+	{
+		argv = &argv[i + 1];
+		i = 0;
+		while (argv[i] && strcmp(argv[i], ";") && strcmp(argv[i], "|"))
+			i ++;
+		if (strcmp(argv[0], "cd") == 0)
+		{
+			if (i != 2)
+				ft_putstr_fd("error: cd: bad arguments", NULL);
+			else if (chdir(argv[i]) != 0)
+				ft_putstr_fd("error: cd: cannot change directory to ", argv[1]);
+		}
+		else if (i != 0 && (argv[i] == NULL || strcmp(argv[i], ";") == 0))
+		{
+			if (fork() == 0)
+			{
+				if (ft_execute(argv, i, tmp_fd, env))
+					return (1);
+			}
+			else
+			{
+				close(tmp_fd);
+				while (waitpid(-1, NULL, WUNTRACED) != -1)
+					;
+				tmp_fd = dup(STDIN_FILENO);
+			}
+		}
+		else if (i != 0 && strcmp(argv[i], "|") == 0)
+		{
+			pipe(fd);
+			if (fork() == 0)
+			{
+				dup2(fd[1], STDOUT_FILENO);
+				close(fd[0]);
+				close(fd[1]);
+				if (ft_execute(argv, i, tmp_fd, env))
+					return (1);
+			}
+			else
+			{
+				close(fd[1]);
+				close(tmp_fd);
+				tmp_fd = fd[0];
+			}
+		}
+	}
+	close(tmp_fd);
+	if (TEST)
+		while (1);
+	return (0);
+}
